7588a72d3a5a9d126972d6b542f48ad88140a2e1
wendy0402 <wendykurniawan92@gmail.com>
2015-07-11 23:36:02 +0700

    add schema for password archivable

--- spec/dummy/db/migrate/password_archivable/20150606093630_sorcery_wand_password_archivable.rb
+++ spec/dummy/db/migrate/password_archivable/20150606093630_sorcery_wand_password_archivable.rb
@@ -2,8 +2,8 @@ class SorceryWandPasswordArchivable < ActiveRecord::Migration
   def change
     create_table :password_archives do |t|
       t.string :crypted_password
-      t.string :salt,
-      t.string :<%= options[:model_name].tableize.singularize %>_id
+      t.string :salt
+      t.string :user_id
 
       t.timestamps null: false
     end
