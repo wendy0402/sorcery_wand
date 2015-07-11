0f628d32fea535c17cc0b5568c6c8964f72c4e3c
wendy0402 <wendykurniawan92@gmail.com>
2015-07-11 23:32:51 +0700

    set default values for config

--- lib/sorcery_wand/config.rb
+++ lib/sorcery_wand/config.rb
@@ -1,11 +1,14 @@
 module SorceryWand
   class Config
-    attr_accessor :submodules
+    attr_accessor :submodules, :user_class
     #password_archivable config
     attr_accessor :password_archivable_count
 
     def initialize
       #set default values
+      @submodules = []
+      @password_archivable_count = 5
     end
+
   end
 end
