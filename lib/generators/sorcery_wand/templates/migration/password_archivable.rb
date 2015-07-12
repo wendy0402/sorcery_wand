cf80156e7ed3148d271ff9a91b417c75cc3f4f66
wendy0402 <wendykurniawan92@gmail.com>
2015-07-12 23:36:45 +0700

    delete comma in generator table password_archivable

--- lib/generators/sorcery_wand/templates/migration/password_archivable.rb
+++ lib/generators/sorcery_wand/templates/migration/password_archivable.rb
@@ -2,7 +2,7 @@ class SorceryWandPasswordArchivable < ActiveRecord::Migration
   def change
     create_table :password_archives do |t|
       t.string :crypted_password
-      t.string :salt,
+      t.string :salt
       t.string :<%= options[:model_name].tableize.singularize %>_id
 
       t.timestamps null: false
