7d442b5df905cd3999558ef3c9f988e4bcab425f
wendy0402 <wendykurniawan92@gmail.com>
2015-07-13 08:17:30 +0700

    disable engine for now because problem with rake db:migrate

--- lib/sorcery_wand/engine.rb
+++ lib/sorcery_wand/engine.rb
@@ -3,8 +3,8 @@ require File.expand_path('../../sorcery_wand.rb', __FILE__)
 
 class SorceryWand::Engine < ::Rails::Engine
   initializer 'register_wand' do
-    if SorceryWand.config.user_class.present?
-      SorceryWand::Submodules.inject_submodules!
-    end
+    # if SorceryWand.config.try(:user_class).present?
+    #   SorceryWand::Submodules.inject_submodules!
+    # end
   end
 end
