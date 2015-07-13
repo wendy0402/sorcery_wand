6f608558a15de0eb7b6faffedb03aa5a350033af
wendy0402 <wendykurniawan92@gmail.com>
2015-07-13 22:40:37 +0700

    delete sorcery wand config in initializer dummy

--- spec/dummy/config/initializers/sorcery_wand.rb
+++
@@ -1,4 +0,0 @@
-SorceryWand.configure do |config|
-  config.submodules = ["password_archivable"]
-  # config.password_archiving_count = 5
-end
--- spec/dummy/config/initializers/sorcery_wand_config.rb
+++
@@ -1,4 +0,0 @@
-SorceryWand.config do |config|
-  # config.feature = [:password_archivable]
-  # config.password_archiving_count = 5
-end
